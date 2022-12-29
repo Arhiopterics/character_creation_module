4b549289a4d0f82a1618d46f7edd3c3f2516d72f
Михаил Козырев <mixailkozireff@yandex.ru>
2022-12-29 18:48:40 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str,
            char_class: str) -> str:
+    """Выдает урон в зависимости от класса."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -19,6 +20,7 @@ def attack(char_name: str,
 
 def defence(char_name: str,
             char_class: str) -> str:
+    """Вычисляет уровень блокировки урона в зависимости от класса."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -30,6 +32,8 @@ def defence(char_name: str,
 
 def special(char_name: str,
             char_class: str) -> str:
+    """В зависимости от типа персонажа возвращает.
+    сообщение о преминение специального умения."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 f'«Выносливость {80 + 25}»')
@@ -42,6 +46,7 @@ def special(char_name: str,
 
 def start_training(char_name: str,
                    char_class: str) -> str:
+    """Запускает цикл тренировки навыков персонажа."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -66,6 +71,7 @@ def start_training(char_name: str,
 
 
 def choice_char_class() -> str:
+    """Позволяет игроку выбрать тип персонажа."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
